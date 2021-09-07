4e49cf8ccbfd27ad9cb3c53677e64008d0a37c9d
Oznnniv <>
2021-09-07 14:39:25 -0500

    New changes

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -390,6 +390,7 @@ function uploadImages(req, next) {
           next(null, error);
           //res.status(500).send(error);
         }else {
+          console.log(dataAWS);
           next(dataAWS.Key, null);
         }
       });
@@ -1137,7 +1138,7 @@ function productorsData(req, res){
       if (err) {
           res.status(500).send({ message: err });
       }else {
-        //console.log(imageName);
+        console.log(imageName);
         req.body.image = imageName;
         serviceInitProductors(req, function(data, err) {
             if (err) {
